fba08e7c32b7bf4090ecb42e0bb50221d0e49e9f
Engr. Peter Y <edwardpeter547@gmail.com>
2024-08-19 00:37:57 +0100

    started with django-rest framework setting up api_view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,16 @@
-from django.http import JsonResponse
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+    """
+    Django-rest framework API View
+    """
     product_data = Product.objects.all().order_by("?").first()
     data = {}
     if product_data:
         data = model_to_dict(product_data, fields=["id", "title", "content", "price"])
-    return JsonResponse(data)
+    return Response(data)
