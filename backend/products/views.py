9e98077b4ac79e8fe284d81ecd702cab966bf1db
Engr. Peter Y <edwardpeter547@gmail.com>
2024-08-25 10:21:09 +0100

    implemented list, retrive and create in a function based view

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,11 @@
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, mixins
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
+from rest_framework import status
 
 
 class ProductDetailApiView(generics.RetrieveAPIView):
@@ -22,3 +27,32 @@ class ProductListCreateApiView(generics.ListCreateAPIView):
 
 
 product_list_create_api_view = ProductListCreateApiView.as_view()
+
+
+@api_view(["POST", "GET"])
+def list_create_view(request, *args, **kwargs):
+    method = request.method
+
+    if method == "GET":
+        pk = kwargs.get("pk", None)
+        if pk is not None:
+            queryset = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(queryset, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+
+    if method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content")
+            if not content:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response(
+            {"invalid": "invalid data, cannot save!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
