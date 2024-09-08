26995d624c3fe9aaa82e90d6205a3300f3e8ecb1
Peter Edward <edwardpeter547@gmail.com>
2024-09-08 06:00:06 +0100

    customizing queryset using request user data

--- backend/api/mixins.py
+++ backend/api/mixins.py
@@ -2,5 +2,15 @@ from api.permissions import IsStaffEditorPermission
 from rest_framework.permissions import IsAdminUser
 
 
-class IsStaffEditorPermissionMixin:
+class StaffEditorPermissionMixin:
     permission_classes = [IsAdminUser, IsStaffEditorPermission]
+
+
+class UserQuerySetMixin:
+    user_field = 'user'
+
+    def get_queryset(self, *args, **kwargs):
+        lookup_data = {}
+        lookup_data[self.user_field] = self.request.user
+        qs = super().get_queryset(*args, **kwargs)
+        return qs.filter(**lookup_data)
