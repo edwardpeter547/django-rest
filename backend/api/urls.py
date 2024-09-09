925582e6e29f9fc7c2f0729457fc37cc4728434e
Peter Edward <edwardpeter547@gmail.com>
2024-09-09 22:14:09 +0100

    implemented jwt tokens

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from api import views
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 app_name = "api"
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path("products/", include("products.urls")),
     path("search/", include("search.urls")),
     path("auth/", obtain_auth_token),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token-verify"),
 ]
